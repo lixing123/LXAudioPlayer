6d2f1599e945c2b9d97a24dca76791e6ec50289f
xing lii <shangwangwanwan@gmail.com>
2016-02-15 22:15:07 +0800

    try to add workspace

--- LXAudioPlayer/LXAudioPlayer/LXAudioPlayer.h
+++ LXAudioPlayer/LXAudioPlayer/LXAudioPlayer.h
@@ -6,6 +6,20 @@
 //  Copyright © 2016年 lixing123.com. All rights reserved.
 //
 
+#ifndef LXHeader_h
+#define LXHeader_h
+
+#define DEBUG_MODE
+
+#ifdef DEBUG_MODE
+#define LXLog(fmt, ...) NSLog((fmt), ##__VA_ARGS__)
+//#define LXLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+#define LXLog(...)
+#endif
+
+#endif /* LXHeader_h */
+
 #import <Foundation/Foundation.h>
 
 //playing state
